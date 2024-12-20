0681dd4380512f9becf51f6be2654abcc097b548
oyku <oykdurmaz@gmail.com>
2024-12-20 09:38:29 +0100

    Initial Commit

---
+++ TaskDone/TaskDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskDoneApp.swift
+//  TaskDone
+//
+//  Created by Öykü Durmaz on 20.12.24.
+//
+
+import SwiftUI
+
+@main
+struct TaskDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskDoneTests/TaskDoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskDoneTests.swift
+//  TaskDoneTests
+//
+//  Created by Öykü Durmaz on 20.12.24.
+//
+
+import Testing
+@testable import TaskDone
+
+struct TaskDoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
